ee14dc7ada382143c2833868f21b6b644e35dbe6
Derek J Johnston <derek@derekjohnston.ca>
2022-05-29 23:09:49 -0400

    fixes for asset images

--- pages/Portfolio/index.vue
+++ pages/Portfolio/index.vue
@@ -1,62 +1,68 @@
 <template lang="pug">
-  .container-fluid 
-    .col-md-12.row
-      .col-md-4(v-for="page in pages")
-        .card
-          .card-header
-            nuxt-link(tag="img" activeClass="active" :to="page.link" :src="page.image" width="100%")
-          .card-body
-            h5.card-title.text-center
-              | {{page.name}}
+.container-fluid 
+  .col-md-12.row
+    .col-md-4(v-for="page in pages")
+      .card
+        .card-header
+          nuxt-link(
+            tag="img",
+            activeClass="active",
+            :to="page.link",
+            :src="require(`~/assets/${page.image}`)",
+            width="100%"
+          )
+        .card-body
+          h5.card-title.text-center
+            | {{ page.name }}
 </template>
 <script>
 export default {
   data() {
-    return  {
-      pages : [
+    return {
+      pages: [
         {
           link: "/Portfolio/Clocks",
-          image: "http://whosjimmy.com/img/clocks/thumbnail/clocks.jpg",
+          image: "clocks/thumbnail/clocks.jpg",
           alt: "Artisan Clocks",
           title: "Artisan Clocks",
-          name: "Artisan Clocks"
+          name: "Artisan Clocks",
         },
         {
           link: "/Portfolio/Machinery",
-          image: "http://whosjimmy.com/img/machinery/thumbnail/may07all%20106.jpg",
+          image: "machinery/thumbnail/may07all 106.jpg",
           alt: "Custom Machinery",
           title: "Custom Machinery",
-          name: "Custom Machinery"
+          name: "Custom Machinery",
         },
         {
           link: "/Portfolio/Celebrities",
-          image: "http://whosjimmy.com/img/thumbnail/IMG_0045-1.jpg",
+          image: "thumbnail/IMG_0045-1.jpg",
           alt: "Jimmy with Celebrities",
           title: "Jimmy with Celebrities",
-          name: "Jimmy with Celebrities"
+          name: "Jimmy with Celebrities",
         },
         {
           link: "/Portfolio/Truck",
-          image: "http://whosjimmy.com/img/thumbnail/truck_mini.jpg",
+          image: "thumbnail/truck_mini.jpg",
           alt: "Set Truck",
           title: "Set Truck",
-          name: "Jimmy's Set Truck"
+          name: "Jimmy's Set Truck",
         },
         {
           link: "/Portfolio/Film",
-          image: "http://whosjimmy.com/img/film/thumbnail/x-men_the_last_stand.jpg",
+          image: "film/thumbnail/x-men_the_last_stand.jpg",
           alt: "Movies and Television",
           title: "Movies and Television",
-          name: "Movies and Television"
+          name: "Movies and Television",
         },
-      ]
-    }
-  }
-}
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-  .card {
-    margin-bottom: 10px;
-  }
+.card {
+  margin-bottom: 10px;
+}
 </style>
