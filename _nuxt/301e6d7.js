92597a93d813f7d83ee3515396bc842be65ae7f9
derekjj <derekjj@users.noreply.github.com>
2022-11-12 02:24:17 +0000

    deploy: b148e627f372725401bde25a6e081792d8372ab7

---
+++ _nuxt/301e6d7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{548:function(t,n,c){"use strict";c.r(n);var e=c(13),component=Object(e.a)({},(function(){this._self._c;return this._m(0)}),[function(){var t=this._self._c;return t("div",{staticClass:"container-fluid"},[t("p",[this._v("Contact")])])}],!1,null,null,null);n.default=component.exports}}]);
