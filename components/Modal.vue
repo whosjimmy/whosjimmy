a36b7f34f3333aa7b810aec687a9ed25a7e01507
Derek J <derek@derekjohnston.ca>
2024-07-11 22:03:08 -0400

    bug fix, font assume icons need height for production

--- components/Modal.vue
+++ components/Modal.vue
@@ -6,30 +6,30 @@
       .close
         button.btn.btn-secondary(title="Close",
               @click="closeCallback()")
-          fab.mr-2.ml-2.fa-2x(:icon="['fa', 'times']")
+          fab.mr-2.ml-2.fa-2x(height="20px" :icon="['fa', 'times']")
       slot
 </template>
 
 <script>
 export default {
-  name: 'Modal',
+  name: "Modal",
   props: {
     show: Boolean,
     customClass: String,
-    closeCallback: Function
-  }
-}
+    closeCallback: Function,
+  },
+};
 </script>
 <style scoped>
-  .modal {
-    width: 100%;
-    background-color: rgba(0, 0, 0, 0.8);
-  }
-  .modal_content {
-    background-color: white;
-  }
-  .close {
-   top:0;
-   right:0;
-  }
+.modal {
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+}
+.modal_content {
+  background-color: white;
+}
+.close {
+  top: 0;
+  right: 0;
+}
 </style>
