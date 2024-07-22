f6bd9e30a00cc9906c09ce2fa2ed30e8b1d7773e
Derek J <derek@derekjohnston.ca>
2024-07-22 11:02:43 -0400

    indent change

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
-  .modal(class="modal",
-        v-bind:class="customClass",
-        v-bind:style="{ display: show ? 'block' : 'none' }")
-    .modal_content
-      .close
-        button.btn.btn-secondary(title="Close",
-              @click="closeCallback()")
-          fab.mr-2.ml-2.fa-2x(height="20px" :icon="['fa', 'times']")
-      slot
+.modal(class="modal",
+      v-bind:class="customClass",
+      v-bind:style="{ display: show ? 'block' : 'none' }")
+  .modal_content
+    .close
+      button.btn.btn-secondary(title="Close",
+            @click="closeCallback()")
+        fab.mr-2.ml-2.fa-2x(height="20px" :icon="['fa', 'times']")
+    slot
 </template>
 
 <script>
