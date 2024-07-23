8f9399d0fe1227cede5b2fda0f9c31c093dca64c
Derek J <derek@derekjohnston.ca>
2024-07-22 20:37:51 -0400

    Nuxt3 upgrade

--- pages/Portfolio/index.vue
+++ pages/Portfolio/index.vue
@@ -2,67 +2,67 @@
 .container-fluid 
   .col-md-12.row
     .col-md-4(v-for="page in pages")
-      .card
+      .card.floating-card
         .card-header
           nuxt-link(
             tag="img",
             activeClass="active",
             :to="page.link",
-            :src="require(`~/assets/${page.image}`)",
             width="100%"
           )
+            img(:src="`/_nuxt/assets/${page.image}`", :alt="page.alt" width="100%")
         .card-body
           h5.card-title.text-center
             | {{ page.name }}
 </template>
 <script>
 export default {
-  data() {
-    return {
-      pages: [
-        {
-          link: "/Portfolio/Clocks",
-          image: "clocks/thumbnail/clocks.jpg",
-          alt: "Artisan Clocks",
-          title: "Artisan Clocks",
-          name: "Artisan Clocks",
-        },
-        {
-          link: "/Portfolio/Machinery",
-          image: "machinery/thumbnail/may07all 106.jpg",
-          alt: "Custom Machinery",
-          title: "Custom Machinery",
-          name: "Custom Machinery",
-        },
-        {
-          link: "/Portfolio/Celebrities",
-          image: "thumbnail/IMG_0045-1.jpg",
-          alt: "Jimmy with Celebrities",
-          title: "Jimmy with Celebrities",
-          name: "Jimmy with Celebrities",
-        },
-        {
-          link: "/Portfolio/Truck",
-          image: "thumbnail/truck_mini.jpg",
-          alt: "Set Truck",
-          title: "Set Truck",
-          name: "Jimmy's Set Truck",
-        },
-        {
-          link: "/Portfolio/Film",
-          image: "film/thumbnail/x-men_the_last_stand.jpg",
-          alt: "Movies and Television",
-          title: "Movies and Television",
-          name: "Movies and Television",
-        },
-      ],
-    };
-  },
-};
+	data() {
+		return {
+			pages: [
+				{
+					link: '/Portfolio/Clocks',
+					image: 'clocks/thumbnail/clocks.jpg',
+					alt: 'Artisan Clocks',
+					title: 'Artisan Clocks',
+					name: 'Artisan Clocks',
+				},
+				{
+					link: '/Portfolio/Machinery',
+					image: 'machinery/thumbnail/may07all 106.jpg',
+					alt: 'Custom Machinery',
+					title: 'Custom Machinery',
+					name: 'Custom Machinery',
+				},
+				{
+					link: '/Portfolio/Celebrities',
+					image: 'thumbnail/IMG_0045-1.jpg',
+					alt: 'Jimmy with Celebrities',
+					title: 'Jimmy with Celebrities',
+					name: 'Jimmy with Celebrities',
+				},
+				{
+					link: '/Portfolio/Truck',
+					image: 'thumbnail/truck_mini.jpg',
+					alt: 'Set Truck',
+					title: 'Set Truck',
+					name: "Jimmy's Set Truck",
+				},
+				{
+					link: '/Portfolio/Film',
+					image: 'film/thumbnail/x-men_the_last_stand.jpg',
+					alt: 'Movies and Television',
+					title: 'Movies and Television',
+					name: 'Movies and Television',
+				},
+			],
+		}
+	},
+}
 </script>
 
 <style scoped>
 .card {
-  margin-bottom: 10px;
+	margin-bottom: 10px;
 }
 </style>
--- plugins/vuelidate.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import Vuelidate from 'vuelidate'
-export default function() {
-  Vue.use(Vuelidate)
-}
-  
