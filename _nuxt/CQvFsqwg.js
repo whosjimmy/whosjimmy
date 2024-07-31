5cb2802aee8c151a531a6344be9a5019b599b6f4
derekjj <derekjj@users.noreply.github.com>
2024-07-31 18:03:31 +0000

    deploy: a4f84b22e30ec639e25a11e2ad4ef89b375eb58f

---
+++ _nuxt/CQvFsqwg.js
@@ -0,0 +1,1 @@
+import{U as o,V as r}from"./Bs_fFaFo.js";import{u as a}from"./BmXJ0crG.js";const s=o((t,u)=>{const e=a();if(!e.user&&t.path!=="/login")return r("/login");if(e.user&&t.path==="/login")return r("/admin")});export{s as default};
