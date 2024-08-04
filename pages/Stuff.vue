0fbc7942027d31386c3fb82ba122ed4e606e942b
Derek J <derek@derekjohnston.ca>
2024-08-04 15:52:56 -0400

    hotfix, reload bug on prod

--- pages/Stuff.vue
+++ pages/Stuff.vue
@@ -11,12 +11,12 @@ import Gallery from '~/components/Gallery.vue'
 export default {
 	data() {
 		return {
-			cover: ['stuff/cars/117-1768_IMG.JPG'],
+			cover: '/stuff/cars/117-1768_IMG.JPG',
 			images: [
-				'stuff/cars/117-1767_IMG.JPG',
-				'stuff/cars/105-0549_IMG.JPG',
-				'stuff/cars/a4 324.JPG',
-				'stuff/cars/IMG_2562.JPG',
+				'/stuff/cars/117-1767_IMG.JPG',
+				'/stuff/cars/105-0549_IMG.JPG',
+				'/stuff/cars/a4 324.JPG',
+				'/stuff/cars/IMG_2562.JPG',
 			],
 			info: ['Cars', 'Neat cars come across and/or owned.'],
 		}
