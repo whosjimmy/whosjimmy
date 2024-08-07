23a9be2564f5ff07d2a6f61bce6270c979be9d70
derekjj <derekjj@users.noreply.github.com>
2024-08-07 01:18:59 +0000

    deploy: 53ee9c06cfe93ea4b181487174ed2c3bd0d4ef01

---
+++ _nuxt/mkOcazfk.js
@@ -0,0 +1,1 @@
+import{V as o,W as r}from"./BkQXSn1D.js";import{u as a}from"./CwPYJ4vx.js";const s=o((t,u)=>{const e=a();if(!e.user&&t.path!=="/login")return r("/login");if(e.user&&t.path==="/login")return r("/admin")});export{s as default};
