c496156874c7748991945e4517c536dfcc8d43d8
Derek J <derek@derekjohnston.ca>
2024-08-20 20:38:53 -0400

    Add an error page

---
+++ error.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="text-center my-0 page">
+		<app-header />
+		<div class="body">
+			<img
+				src="\film\body_parts\vlcsnap-2010-10-12-10h36m24s49.png"
+				alt="Error Image"
+			/>
+			<h2>Error: {{ error?.statusCode }}</h2>
+			<button class="btn btn-primary" @click="handleError">
+				Take me home
+			</button>
+		</div>
+		<app-footer />
+	</div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+const props: any = defineProps({
+	error: Object as () => NuxtError,
+})
+const { $config } = useNuxtApp()
+
+useHead({
+	title: props?.error?.statusCode.toString() || 'Error',
+})
+const handleError = () => {
+	useHead({
+		title: $config.public.SITE_TITLE,
+	})
+	clearError({ redirect: '/' })
+}
+</script>
+
+<script lang="ts">
+import Header from '~/components/Header.vue'
+import Footer from './components/Footer.vue'
+export default {
+	head() {
+		return {
+			title: 'Error',
+		}
+	},
+	components: {
+		appHeader: Header,
+		appFooter: Footer,
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+	max-width: 50%;
+	height: auto;
+}
+</style>
