ab62a7aef49741421b60c44a776871262a42ca5d
Derek J <derek@derekjohnston.ca>
2024-08-20 20:47:11 -0400

    update layout and add admin header for admin pages

---
+++ components/Admin/Header.vue
@@ -0,0 +1,109 @@
+<template lang="pug">
+div
+  nav.navbar.navbar-expand-md.navbar-light.bg-light
+    nuxt-link.navbar-brand(to="/Admin/Home")
+      | Home
+    button.navbar-toggler(
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      @click="visible=!visible")
+      span.navbar-toggler-icon
+    .navbar-collapse(id="navbarNavDropdown" :class="!visible?'collapse':''" )
+      ul.navbar-nav
+        nuxt-link.btn.btn-light.nav-item(
+          tag="li",
+          activeClass="active",
+          to="/Admin/Bio")
+          | Bio
+        nuxt-link.btn.btn-light.nav-item(
+          tag="button",
+          activeClass="active",
+          to="/Admin/Portfolio")
+          | Portfolio               
+        nuxt-link.btn.btn-light.nav-item(
+          tag="button",
+          activeClass="active",
+          to="/Admin/ForSale")
+            | For Sale
+        nuxt-link.btn.btn-light.nav-item(tag="li" activeClass="active" to="/Admin/Stuff")
+          | Cool Stuff
+        nuxt-link.btn.btn-light.nav-item(tag="li" activeClass="active" to="/Admin/Contact")
+          | Contact Us
+  nav.navbar.navbar-expand-md.navbar-light.bg-light(v-if="$route.path.startsWith('/Admin/Portfolio')")
+    nuxt-link.btn.btn-light.nav-item(
+      tag="button",
+      activeClass="active",
+      to="/Admin/Portfolio/Clocks")
+      | Clocks
+    nuxt-link.btn.btn-light.nav-item(
+      tag="button",
+      activeClass="active",
+      to="/Admin/Portfolio/Machinery")
+      | Machinery
+    nuxt-link.btn.btn-light.nav-item(
+      tag="button",
+      activeClass="active",
+      to="/Admin/Portfolio/Celebs")
+      | Celebs
+    nuxt-link.btn.btn-light.nav-item(
+      tag="button",
+      activeClass="active",
+      to="/Admin/Portfolio/Truck")
+      | Truck
+    nuxt-link.btn.btn-light.nav-item(
+      tag="button",
+      activeClass="active",
+      to="/Admin/Portfolio/Film")
+      | Film
+    nuxt-link.btn.btn-light.nav-item(
+      tag="button",
+      activeClass="active",
+      to="/Admin/Portfolio/Videos")
+      | Videos
+  nav.navbar.navbar-expand-md.navbar-light.bg-light(v-if="$route.path.startsWith('/Admin/ForSale')")
+    nuxt-link.btn.btn-light.nav-item(
+      tag="button",
+      activeClass="active",
+      to="/Admin/ForSale")
+        | For Sale
+    nuxt-link.btn.btn-light.nav-item(tag="li" activeClass="active" to="/Admin/Stuff")
+      | Cool Stuff
+    nuxt-link.btn.btn-light.nav-item(tag="li" activeClass="active" to="/Admin/Contact")
+      | Contact Us
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia'
+import { useAuthStore } from '~/stores/auth'
+
+export default {
+	data() {
+		return {
+			visible: false,
+			isAuth: false,
+		}
+	},
+	computed: {
+		...mapState(useAuthStore, {
+			user: 'getUser',
+			isAuthenticated: 'isAuthenticated',
+		}),
+	},
+	watch: {
+		isAuthenticated(newVal) {
+			this.isAuth = newVal
+		},
+	},
+	mounted() {
+		this.isAuth = this.isAuthenticated
+	},
+
+	methods: {
+		...mapActions(useAuthStore, { signOut: 'signOut' }),
+	},
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,57 @@
 <template lang="pug">
-.text-center.my-0
+.text-center.my-0.page
 	app-header
-	br
-	.body
+	app-admin-header(v-if="$route.path.startsWith('/Admin')" class="pt-1")
+	.body.mt-3(v-if="!getLoading")
 		NuxtPage
-	br
+	.body(v-else)
+		app-loader(:loading="getLoading") 
 	app-footer
 	app-cookie-consent
 </template>
 <script setup>
 import { useAuthStore } from '~/stores/auth'
 import { useDepartmentStore } from '~/stores/departments'
+import { useGeneralStore } from '~/stores/general'
+import Loader from '~/components/Loader.vue'
 
 const authStore = useAuthStore()
 const departmentStore = useDepartmentStore() // Use the Pinia store
+const generalStore = useGeneralStore()
 
-authStore.initializeAuth()
-departmentStore.fetchDepartments()
+Promise.all([
+	departmentStore.fetchDepartments(),
+	generalStore.fetchGeneral(),
+	generalStore.fetchTrackingCookie(),
+	authStore.initializeAuth(),
+]).then(() => {
+	authStore.setLoading(false)
+})
 </script>
 <script>
 import Header from '~/components/Header.vue'
+import AdminHeader from '~/components/Admin/Header.vue'
 import Footer from '~/components/Footer.vue'
 import CookieConsent from '~/components/CookieConsent.vue'
+import { mapState } from 'pinia'
 export default {
 	components: {
 		appHeader: Header,
+		appAdminHeader: AdminHeader,
 		appFooter: Footer,
 		appCookieConsent: CookieConsent,
+		appLoader: Loader,
+	},
+	computed: {
+		...mapState(useAuthStore, { getLoading: 'getLoading' }),
+	},
+	mounted() {
+		console.log(this.$route.path)
 	},
 }
 </script>
 
 <style>
-body {
-	padding-top: 0px;
-}
 .floating-card {
 	background-color: white;
 	box-shadow:
@@ -51,39 +68,23 @@ body {
 		0 8px 16px rgba(0, 0, 0, 0.2),
 		0 12px 40px rgba(0, 0, 0, 0.2); /* Enhanced shadow on hover */
 }
-</style>
-
-<style scoped>
+.page-enter-active,
+.page-leave-active {
+	transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+	opacity: 0;
+	filter: blur(1rem);
+}
 .body {
 	max-width: 1000px;
 	margin: auto;
 }
-
-.slide-enter-active {
-	animation: slide-in 300ms ease-out forwards;
-}
-.slide-leave-active {
-	animation: slide-out 300ms ease-out forwards;
-}
-
-@keyframes slide-in {
-	from {
-		transform: translateY(-30px);
-		opacity: 0;
-	}
-	to {
-		transform: translateY(0);
-		opacity: 1;
-	}
-}
-@keyframes slide-out {
-	from {
-		transform: translateY(0);
-		opacity: 1;
-	}
-	to {
-		transform: translateY(-30px);
-		opacity: 0;
-	}
+.page {
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	min-width: 95vw;
 }
 </style>
