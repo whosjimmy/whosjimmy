ccb2d392b6df57ab0f34c0b47f4b782b71dc9e9e
Derek J <derek@derekjohnston.ca>
2024-08-20 20:51:33 -0400

    update homepage to use store / firebase storage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,34 @@
 <template lang="pug">
-.container-fluid
-  .card
-    .card-header
-      img(src="~assets/JIMMY_CLAWS.jpg" width="100%")
-
+div
+	.splash(v-if="getGeneral[0]?.cover" :style="{backgroundImage: 'url(' + getGeneral[0]?.cover + ')'}")
+	.splash.splash2(v-else)
 </template>
+
+<script>
+import { mapState } from 'pinia'
+import { useGeneralStore } from '~/stores/general'
+export default {
+	data() {},
+	computed: {
+		...mapState(useGeneralStore, ['getGeneral']),
+	},
+	mounted() {},
+}
+</script>
+<style scoped>
+.splash {
+	position: fixed;
+	top: 20px;
+	bottom: 100px;
+	left: 0px;
+	right: 0px;
+	z-index: -1;
+	background-size: contain;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-color: #000;
+}
+.splash2 {
+	background-image: url('~/assets/JIMMY_CLAWS.jpg');
+}
+</style>
