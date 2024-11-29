7ca1939a3fb50f742170f9d30795b87fa7874a08
derekjj <derekjj@users.noreply.github.com>
2024-11-29 02:53:03 +0000

    deploy: d8eb97e0272786ce3c7325ed1e7ea5a0414890ee

---
+++ _nuxt/zBB1J6jC.js
@@ -0,0 +1,1 @@
+import{V as r,u as o,W as a}from"./CbWZRH0g.js";const n=r((t,u)=>{const e=o();if(!e.user&&t.path!=="/login")return a("/login");if(e.user&&t.path==="/login")return a("/admin")});export{n as default};
