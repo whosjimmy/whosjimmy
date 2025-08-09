4f7134ea35504fda799f4e0130033b9759f2110e
Derek J <derek@derekjohnston.ca>
2025-08-08 21:35:11 -0400

    Allow for hover effect and gallery

--- pages/Portfolio/Film.vue
+++ pages/Portfolio/Film.vue
@@ -1,33 +1,90 @@
-<template lang="pug">
-.container-fluid.col-lg-12
-  .card.rounded.text-center
-    .card-header
-      | Portfolio - Movies and Television
-    .card-body
-      .row
-        .col-lg-12(v-if="!sortedFilms?.length")
-          .card
-            .card-header
-              | Loading
-        .col-md-4.my-1(v-for="film in sortedFilms")
-          .card.h-100
-            .card-header.app-gallery(v-if="film?.Photos && film?.Photos?.length > 0")
-              appGallery(:name="film.Name", :images="film.Photos", :cover="film.Image")
-            .card-header(v-else  @click="gtmGalleryViewedFailedEvent(film)")
-              img.card-img-top(:src="film?.Image")
-            .card-body
-              h5.card-title.text-center(v-if="film.Years[0] || film.Years[1]")
-                span(v-if="film.Years[0]") {{film.Years[0]}}
-                span(v-if="film.Years[1]") &nbsp;- {{film.Years[1]}}
-                br(v-if="film.Name")
-                | {{film.Name}}
-              p.card-text.text-center(v-if="film.Department")
-                | ({{film.Department}})
-                a.imdb(:href="film.Link" target="_blank" v-if="film.Link")
-                  br
-                  font-awesome-icon.mr-2.ml-2.fa-2x(height="20px", :icon="['fab', 'imdb']")
-                //- br(v-if="film.Description")
-                //- | ({{film.Description}})
+<template>
+	<div class="container-fluid col-lg-12">
+		<div class="card rounded text-center">
+			<div class="card-header">Portfolio - Movies and Television</div>
+			<div class="card-body">
+				<div class="row">
+					<div v-if="!sortedFilms?.length" class="col-lg-12">
+						<div class="card">
+							<div class="card-header">Loading</div>
+						</div>
+					</div>
+					<div
+						v-for="film in sortedFilms"
+						:key="film.Name"
+						class="col-md-4 my-1"
+					>
+						<div
+							class="card h-100"
+							:class="{
+								'floating-card':
+									film?.Photos &&
+									film?.Photos?.length > 0 &&
+									!activeGallery,
+								'gallery-open': activeGallery === film.Name,
+							}"
+						>
+							<div
+								v-if="film?.Photos && film?.Photos?.length > 0"
+								class="card-header app-gallery"
+							>
+								<appGallery
+									:name="film.Name"
+									:images="film.Photos"
+									:cover="film.Image"
+									@gallery-opened="onGalleryOpened"
+									@gallery-closed="onGalleryClosed"
+								/>
+							</div>
+							<div
+								v-else
+								class="card-header"
+								@click="gtmGalleryViewedFailedEvent(film)"
+							>
+								<img class="card-img-top" :src="film?.Image" />
+							</div>
+							<div class="card-body">
+								<h5
+									v-if="film.Years[0] || film.Years[1]"
+									class="card-title text-center"
+								>
+									<span v-if="film.Years[0]">{{
+										film.Years[0]
+									}}</span>
+									<span v-if="film.Years[1]"
+										>&nbsp;- {{ film.Years[1] }}</span
+									>
+									<br v-if="film.Name" />
+									{{ film.Name }}
+								</h5>
+								<p
+									v-if="film.Department"
+									class="card-text text-center"
+								>
+									({{ film.Department }})
+									<a
+										v-if="film.Link"
+										class="imdb"
+										:href="film.Link"
+										target="_blank"
+									>
+										<br />
+										<font-awesome-icon
+											class="mr-2 ml-2 fa-2x"
+											height="20px"
+											:icon="['fab', 'imdb']"
+										/>
+									</a>
+									<!-- <br v-if="film.Description" /> -->
+									<!-- ({{ film.Description }}) -->
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -36,17 +93,21 @@ import { useDepartmentStore } from '~/stores/departments'
 import Gallery from '~/components/Gallery.vue'
 import films from '~/public/film.json'
 export default {
+	components: {
+		appGallery: Gallery,
+	},
 	data() {
 		return {
 			films,
 			sortedFilms: [],
+			activeGallery: null, // Track which gallery is currently open
 		}
-	},
-	created() {
-		this.processMovies()
 	},
 	computed: {
 		...mapState(useDepartmentStore, ['departments', ['departments']]),
+	},
+	created() {
+		this.processMovies()
 	},
 	methods: {
 		//TODO: confirm events captured on analytics
@@ -56,6 +117,12 @@ export default {
 				film_name: film.Name,
 				page_path: '/portfolio/film',
 			})
+		},
+		onGalleryOpened(galleryName) {
+			this.activeGallery = galleryName
+		},
+		onGalleryClosed() {
+			this.activeGallery = null
 		},
 		processMovies() {
 			let allMovies = []
@@ -85,9 +152,6 @@ export default {
 			})
 		},
 	},
-	components: {
-		appGallery: Gallery,
-	},
 }
 </script>
 
@@ -110,5 +174,34 @@ export default {
 } */
 .app-gallery {
 	cursor: pointer;
+}
+
+/* Floating card effect - only applied to cards with photos when no gallery is open */
+.floating-card {
+	background-color: white;
+	box-shadow:
+		0 4px 8px rgba(0, 0, 0, 0.1),
+		0 6px 20px rgba(0, 0, 0, 0.1); /* Soft shadow for floating effect */
+	transition:
+		transform 0.2s,
+		box-shadow 0.2s; /* Smooth transition for hover effect */
+}
+
+.floating-card:hover {
+	transform: translateY(-10px); /* Slight lift on hover */
+	box-shadow:
+		0 8px 16px rgba(0, 0, 0, 0.2),
+		0 12px 40px rgba(0, 0, 0, 0.2); /* Enhanced shadow on hover */
+}
+
+/* Ensure no floating effect when gallery is open */
+.gallery-open {
+	transform: none !important;
+	transition: none !important;
+}
+
+.gallery-open:hover {
+	transform: none !important;
+	box-shadow: initial !important;
 }
 </style>
